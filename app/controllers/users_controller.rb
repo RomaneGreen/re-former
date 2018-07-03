3c8160e4ed7d694d4b52503f63df247a8f8ef289
Romane <romane71193@gmail.com>
2018-07-03 02:36:21 -0400

    fix: routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,23 @@ class UsersController < ApplicationController
    if @user.save
     redirect_to new_user_path
     else
-    render :new
+    render 'new'
    end
   end
 
 def edit
  @user = User.find(params[:id])
+
+render 'new'
 end
+
+
+def update
+  @user = User.find(params[:id])
+  @user.update(username: params[:username], email: params[:email],password: params[:password])
+
+end
+
   private
 
    # gives us back just the hash containing the params we need to
